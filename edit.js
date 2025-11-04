af66bb5d5bf62370e992869b994f3fba752c6520
Yusuf-AI2011 <yusufshomurodov36@gmail.com>
2025-11-04 12:10:30 +0500

    Variables

---
+++ edit.js
@@ -0,0 +1,405 @@
+//number
+
+var number1 = 1;
+console.log('1st number is - ' + number1);
+
+var number2 = 2;
+console.log('2nd number is - ' + number2);
+
+var number3 = 3;
+console.log('3rd number is - ' + number3);
+
+var number4 = 4;
+console.log('4th number is - ' + number4);
+
+var number5 = 5;
+console.log('5th number is - ' + number5);
+
+var number6 = 6;
+console.log('6th number is - ' + number6);
+
+var number7 = 7;
+console.log('7th number is - ' + number7);
+
+var number8 = 8;
+console.log('8th number is - ' + number8);
+
+var number9 = 9;
+console.log('9th number is - ' + number9);
+
+var number10 = 10;
+console.log('10th number is - ' + number10);
+
+var number11 = 11;
+console.log('11th number is - ' + number11);
+
+var number12 = 12;
+console.log('12th number is - ' + number12);
+
+var number13 = 13;
+console.log('13th number is - ' + number13);
+
+var number14 = 14;
+console.log('14th number is - ' + number14);
+
+var number15 = 15;
+console.log('15th number is - ' + number15);
+
+var number16 = 16;
+console.log('16th number is - ' + number16);
+
+var number17 = 17;
+console.log('17th number is - ' + number17);
+
+var number18 = 18;
+console.log('18th number is - ' + number18);
+
+var number19 = 19;
+console.log('19th number is - ' + number19);
+
+var number20 = 20;
+console.log('20th number is - ' + number20);
+
+// string
+
+var isName = prompt('What is your name?')
+console.log('Your name is - ' + isName);
+
+var isSur = prompt('What is your surname?')
+console.log('Your surname is - ' + isSur);
+
+var isAge = +prompt('How old are you?')
+console.log('You are - ' + isAge + ' years old');
+
+var isJob = prompt('What is your job?')
+console.log('Yous are - ' + isJob);
+
+var isHob = prompt('What is your hobby?')
+console.log('Your hobby is - ' + isHob);
+
+var isGen = prompt('What is your gender?')
+console.log('You are - ' + isGen);
+
+var isCountry = prompt('WHere are you from?')
+console.log('You are from - ' + isCountry);
+
+var isNum = prompt('What is your favourite number?')
+console.log('Your favourite number is - ' + isNum);
+
+var isColor = prompt('What is your favourite colour?')
+console.log('Your favourite colour is - ' + isColor);
+
+var isWeight = prompt('What is your weight?')
+console.log('Your weight is - ' + isWeight);
+
+var isHeight = prompt('What is your height?')
+console.log('Your height is  - ' + isHeight);
+
+var isPet  = prompt('What pet do you have?')
+console.log('Your pet is - ' + isPet);
+
+var isWeather = prompt('What is your favourite weather?')
+console.log('Your favourite weather is - ' + isWeather);
+
+var isTime = prompt('What is your favourite time at clock?')
+console.log('Your favourite time is - ' + isTime);
+
+var isDay = prompt('What is your favourite day?')
+console.log('Your favourite day is - ' + isDay);
+
+var isDate  = prompt('What is your favourite date?')
+console.log('Your favourite date is - ' + isDate);
+
+var isBook = prompt('What is your favourite book?')
+console.log('Your favourite book is - ' + isBook);
+
+var isSport = prompt('What is your favourite sport?')
+console.log('Your favourite sport is - ' + isSport);
+
+var isPlanet = prompt('What is your favourite planet?')
+console.log('Your favourite planet is' + isPlanet);
+
+var isFilm = prompt('What is your favourite film?')
+console.log('Your favourite film is - ' + isFilm);
+
+// boolean
+
+alert('Firstly (1 - true) and (0 - false)')
+
+
+var bool = prompt('Choose correct answer: sinA = 0.5archtag*(-34) ')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 6 = 18')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 13 + 5 = 18')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 15 + 5 = 20')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 9 + 5 = 14')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 7 = 19')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 11 + 5 = 16')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 5 + 11 = 16')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 0 + 5 = 5')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+var bool = prompt('Choose correct answer: 5 + 17 = 22')
+if (bool == 1) {
+    console.log('correct');
+}
+
+else {
+    console.log('not coorect');
+}
+
+// 1 - part
+
+var bool = prompt('Choose correct answer: 12 + 5 = 12')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 11')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 10')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 13')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 178584879384759873')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 17734963745673874')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 179598702398497')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 17873598734')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 1793485078376')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+var bool = prompt('Choose correct answer: 12 + 5 = 1783459734857')
+if (bool == 1) {
+    console.log('not correct');
+}
+
+else {
+    console.log('coorect');
+}
+
+// 2 - part
+
+// null
+
+var box1 = null;
+var box2 = null;
+var box3 = null;
+var box4 = null;
+var box5 = null;
+var box6 = null;
+var box7 = null;
+var box8 = null;
+var box9 = null;
+var box10 = null;
+var box11 = null;
+var box12 = null;
+var box13 = null;
+var box14 = null;
+var box15 = null;
+var box16 = null;
+var box17 = null;
+var box18 = null;
+var box19 = null;
+var box20 = null;
+
+// undefined
+
+var grade1 = undefined;
+var grade2 = undefined;
+var grade3 = undefined;
+var grade4 = undefined;
+var grade5 = undefined;
+var grade6 = undefined;
+var grade7 = undefined;
+var grade8 = undefined;
+var grade9 = undefined;
+var grade10 = undefined;
+var grade11 = undefined;
+var grade12 = undefined;
+var grade13 = undefined;
+var grade14 = undefined;
+var grade15 = undefined;
+var grade16 = undefined;
+var grade17 = undefined;
+var grade18 = undefined;
+var grade19 = undefined;
+var grade20 = undefined;
+
+// symbol
+
+var sym1 = Symbol("A123");
+var sym2 = Symbol("B123");
+var sym3 = Symbol("C123");
+var sym4 = Symbol("D123");
+var sym5 = Symbol("E123");
+var sym6 = Symbol("F123");
+var sym7 = Symbol("G123");
+var sym8 = Symbol("H123");
+var sym9 = Symbol("I123");
+var sym10 = Symbol("J123");
+var sym11 = Symbol("K123");
+var sym12 = Symbol("L123");
+var sym13 = Symbol("M123");
+var sym15 = Symbol("N123");
+var sym16 = Symbol("O123");
+var sym17 = Symbol("P123");
+var sym18 = Symbol("Q123");
+var sym19 = Symbol("R123");
+var sym20 = Symbol("S123");
+
+
+// bigint
+
+var num1 = BigInt(762736426348624376928734962384675n);
+var num2 = BigInt(82347671297648723867265848734956n);
+var num3 = BigInt(8757698234976283596287349768374723654645154n);
+var num4 = BigInt(9846580238477365869827496283657643756n);
+var num5 = BigInt(5468431346354135468435138468415168345687461384641n)
+var num6 = BigInt(44231554334515334453531453546n);
+var num6 = BigInt(546245124541205481525555555555555555222222n)
+var num7 = BigInt(11111111111111111111122222222222222222222222221212222222222222222222222221212121212n);
+var num8 = BigInt(3948579385976938476939489869723567023756n);
+var num9 = BigInt(8798372586270294729735698802497296359828350698569745n);
+var num10 = BigInt(27387945972349762598776238769856n);
+var num11 = BigInt(4375938592345976235732465674343647366734762764367768n);
+var num12 = BigInt(2757693797623857963589632323535253253n);
+var num13 = BigInt(8723975692357682346927368658766835473926346776476764357676854n);
+var num14 = BigInt(8934573245462332314543230524325153254n);
+var num15 = BigInt(2365769264576852368235763765876325732737667367n)
+var num16 = BigInt(873543456443315554468441354564454153454684456n);
+var num17 = BigInt(2454623413344684321644382435313233544545642354n);
+var num18 = BigInt(3546343546832845234453543245153433544353141341n);
+var num19 = BigInt(82739569273702352456874333153532153523455433251353n);
+var num20 = BigInt(82735727349772947239856727085627305869823570863n)
